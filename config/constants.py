3ecd2c99e03b9d0803656723d767c7f98080892d
Gabriel Tsai <gabrielhtsai@gmail.com>
2025-02-08 19:54:43 -0800

    more constants

--- config/constants.py
+++ config/constants.py
@@ -1,2 +1,3 @@
 TILE_WIDTH = 10 # 10 degrees tile width for GHSL
 MAX_RATIO = 0.7 # Maximum ratio of black pixels to non-black pixels for patch to be considered
+BUILDINGS_THRESHOLD = 100 # GHSL scores settlement between 0 and 100000. We consider a pixel to be a building if it has a score greater than 100
