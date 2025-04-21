eafd7cdfa9ac86b53133c7ed0ea316b381b4c248
Gabriel Tsai <gthtsai@sc.stanford.edu>
2025-04-21 14:28:51 -0700

    buggy huggingface upload

--- util/process_bright.py
+++ util/process_bright.py
@@ -1,11 +1,12 @@
 import os
+import shutil
 from huggingface_hub import create_repo, repo_exists
 from datasets import Dataset, DatasetDict, config, Features, Image, Value
 
 import argparse
 
 TRAIN_DIS = set(['bata-explosion', 'la_palma-volcano', 'turkey-earthquake', 'beirut-explosion', 'congo-volcano', 'haiti-earthquake', 'hawaii-wildfire'])
-VAL_DIS = set(['libya-flood', 'morroco-earthquake'])
+VAL_DIS = set(['libya-flood', 'morocco-earthquake'])
 
 def create_split_dirs(base_dir, split_name):
     """
@@ -33,9 +34,9 @@ def rearrange_bright(bright_dir, data_dir):
     
     create_split_dirs(data_dir, 'train')
     create_split_dirs(data_dir, 'val')
-
+    print(os.listdir(bright_dir))
     post_dir, pre_dir, target_dir = sorted(os.listdir(bright_dir))
-
+    
     for pre_img, post_img, target_img in zip(
         sorted(os.listdir(os.path.join(bright_dir, pre_dir))),
         sorted(os.listdir(os.path.join(bright_dir, post_dir))),
@@ -51,46 +52,52 @@ def rearrange_bright(bright_dir, data_dir):
             raise ValueError(f"Unknown disaster {disaster} in {target_img}.")
         
         pre_img_path = os.path.join(bright_dir, pre_dir, pre_img)
-        new_pre_img_path = os.path.join(split_path, 'images', pre_img)
-        os.move(pre_img_path, new_pre_img_path)
+        new_pre_img_path = os.path.join(split_path, 'images', pre_img)        
+        shutil.move(pre_img_path, new_pre_img_path)
 
         post_img_path = os.path.join(bright_dir, post_dir, post_img)
         new_post_img_path = os.path.join(split_path, 'images', post_img)
-        os.move(post_img_path, new_post_img_path)
+        shutil.move(post_img_path, new_post_img_path)
         
         new_target_img = target_img[:target_img.find('_building_damage')] + '_target.tif'
         target_img_path = os.path.join(bright_dir, target_dir, target_img)
         new_target_img_path = os.path.join(split_path, 'targets', new_target_img)
-        os.move(target_img_path, new_target_img_path)
+        shutil.move(target_img_path, new_target_img_path)
 
 def upload_to_hf(data_dir, repo_name = "BRIGHT-XView2Format"):
     if not repo_exists(repo_name, repo_type='dataset'):
         create_repo(repo_name, repo_type="dataset", private=False)
-        print("balls")
     else:
         print(f"Repo {repo_name} already exists.")
     
-    pairs = {}
-    for dataset in os.listdir(data_dir):
+    splits = {}
+    for dataset in ('train', 'val'):
         images_dir = os.path.join(data_dir, dataset, 'images')
         targets_dir = os.path.join(data_dir, dataset, 'targets')
-        pairs[dataset] = []
-        for img_name in zip(sorted(os.listdir(images_dir)), sorted(os.listdir(targets_dir))):
-            img_path = os.path.join(images_dir, img_name)
-            target_path = os.path.join(targets_dir, img_name)
-            pairs[dataset].append({'image': img_path, 'target': target_path})
+        splits[dataset] = []
+        for img_name, target_name in zip(sorted(os.listdir(images_dir)), sorted(os.listdir(targets_dir))):
+            dis_name_code = ''
+            if 'pre' in img_name:      
+                dis_name_code = img_name[:img_name.find('_pre')]
+            else:    
+                dis_name_code = img_name[:img_name.find('_post')]
+            pre_img_path = os.path.join(images_dir, dis_name_code + '_pre_disaster.tif') 
+            post_img_path = os.path.join(images_dir, dis_name_code + '_post_disaster.tif')
+            target_path = os.path.join(targets_dir, target_name)
+            splits[dataset].append({'pre_disaster_image': pre_img_path, 'post_disaster_image': post_img_path, 'target': target_path})
     
     features = Features({
-        'image': Image(),
+        'pre_disaster_image': Image(),
+        'post_disaster_image': Image(), 
         'target': Image()
     })
 
-    dataset = DatasetDict({
-        'train': Dataset.from_list(pairs[dataset], features=features),
-        'val': Dataset.from_list(pairs[dataset], features=features)
+    dataset_hf = DatasetDict({
+        'train': Dataset.from_list(splits['train'], features=features),
+        'val': Dataset.from_list(splits['val'], features=features)
     }) 
 
-    dataset.push_to_hub(repo_name, private=False)
+    dataset_hf.push_to_hub(repo_name, private=False)
 
 def main():
     parser = argparse.ArgumentParser(description="Uploading BRIGHT w/ XView2 Format to HF.")
