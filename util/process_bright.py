eb06fc879ec1bd42c0c5cd9588e199e9dcd67633
Gabriel Tsai <gthtsai@atlas23.stanford.edu>
2025-09-27 16:07:28 -0700

    bright uploading

--- util/process_bright.py
+++ util/process_bright.py
@@ -2,9 +2,16 @@ import os
 import shutil
 from huggingface_hub import create_repo, repo_exists
 from datasets import Dataset, DatasetDict, config, Features, Image, Value
+from huggingface_hub.utils import logging as hfhub_logging
 import tifffile as tiff
 
 import argparse
+
+from tqdm import tqdm
+
+import psutil
+
+hfhub_logging.set_verbosity_debug()
 
 TRAIN_DIS = set(['bata-explosion', 'la_palma-volcano', 'turkey-earthquake', 'beirut-explosion', 'congo-volcano', 'haiti-earthquake', 'hawaii-wildfire'])
 VAL_DIS = set(['libya-flood', 'morocco-earthquake'])
@@ -56,19 +63,18 @@ def create_class_finetune(bright_dir, data_dir):
             img_path = os.path.join(data_dir, split, event_name, img_name)
             shutil.move(os.path.join(bright_split_dir, 'post-event', img_name), img_path)
 
-def rearrange_bright(bright_dir, data_dir):
+def rearrange_bright(bright_dir, data_dir, split):
     """
-    Reorganize bright dataset train folder into train/val split following torchange xview2 structure.
-    Split is done based on disaster, with most recent disasters in validation dataset to prevent
-    temporal leakage.
+    Reorganize bright dataset split folder following torchange xview2 structure.
 
     Args:
-        bright_dir (str): Directory containing the bright dataset.
+        bright_dir (str): Directory containing the split for the bright dataset.
         data_dir (str): Directory to save the rearranged dataset.
     """
-    
-    create_split_dirs(data_dir, 'train')
-    create_split_dirs(data_dir, 'val')
+    if split not in ['train', 'val', 'test']: raise ValueError("Invalid split")
+ 
+    if not os.path.exists(os.path.join(data_dir, split)):
+        create_split_dirs(data_dir, split)
     print(os.listdir(bright_dir))
     post_dir, pre_dir, target_dir = sorted(os.listdir(bright_dir))
     
@@ -79,12 +85,7 @@ def rearrange_bright(bright_dir, data_dir):
         ):
         disaster = target_img[:target_img.find('_0')]
         
-        if disaster in TRAIN_DIS:
-            split_path = os.path.join(data_dir, 'train')
-        elif disaster in VAL_DIS:
-            split_path = os.path.join(data_dir, 'val')
-        else:
-            raise ValueError(f"Unknown disaster {disaster} in {target_img}.")
+        split_path = os.path.join(data_dir, split)
         
         pre_img_path = os.path.join(bright_dir, pre_dir, pre_img)
         new_pre_img_path = os.path.join(split_path, 'images', pre_img)        
@@ -98,39 +99,54 @@ def rearrange_bright(bright_dir, data_dir):
         target_img_path = os.path.join(bright_dir, target_dir, target_img)
         new_target_img_path = os.path.join(split_path, 'targets', new_target_img)
         shutil.move(target_img_path, new_target_img_path)
-
+        
 def upload_to_hf(data_dir, repo_name = "BRIGHT-XView2Format"):
+    os.chdir(data_dir)
+    assert os.path.exists("train/images/bata-explosion_00000000_pre_disaster.tif"), "Image path not valid" 
     if not repo_exists(repo_name, repo_type='dataset'):
         create_repo(repo_name, repo_type="dataset", private=False)
     else:
         print(f"Repo {repo_name} already exists.")
     
     splits = {}
-    for dataset in ('train', 'val'):
+    for dataset in tqdm(('train', 'val', 'test')):
         images_dir = os.path.join(data_dir, dataset, 'images')
         targets_dir = os.path.join(data_dir, dataset, 'targets')
+        print(images_dir, targets_dir)
         splits[dataset] = []
-        for img_name, target_name in zip(sorted(os.listdir(images_dir)), sorted(os.listdir(targets_dir))):
-            event_name = img_name[:img_name.find('_0')]
-            if event_name not in TRAIN_DIS and event_name not in VAL_DIS:
-                raise ValueError(f"Unknown disaster {event_name} in {img_name}.")
-            dis_name_code = ''
-            if 'pre' in img_name:      
-                dis_name_code = img_name[:img_name.find('_pre')]
-            else:    
-                dis_name_code = img_name[:img_name.find('_post')]
-            pre_img_path = os.path.join(images_dir, dis_name_code + '_pre_disaster.tif') 
-            post_img_path = os.path.join(images_dir, dis_name_code + '_post_disaster.tif')
+        for target_name in tqdm(sorted(os.listdir(targets_dir))):
+            event_full_name = target_name[:target_name.find('_target')]
+            event_name = target_name[:target_name.find('_0')]
+            pre_img_path = os.path.join(images_dir, event_full_name + '_pre_disaster.tif') 
+            post_img_path = os.path.join(images_dir, event_full_name + '_post_disaster.tif')
             target_path = os.path.join(targets_dir, target_name)
+            
+            pre_img_rel_path = os.path.relpath(pre_img_path, start = data_dir) 
+            post_img_rel_path = os.path.relpath(post_img_path, start = data_dir) 
+            target_img_rel_path = os.path.relpath(target_path, start = data_dir)
+            if not (os.path.exists(os.path.join(data_dir, pre_img_rel_path)) and
+                os.path.exists(os.path.join(data_dir, post_img_rel_path)) and
+                os.path.exists(os.path.join(data_dir, target_img_rel_path))):
+                raiseValueError("Path(s) do not exist")
+            #pre_img = tiff.imread(pre_img_path)
+            #post_img = tiff.imread(post_img_path)
+            #target = tiff.imread(target_path)
+            #
+            #splits[dataset].append({'t1_image': pre_img, 't2_image': post_img, 
+            #                        'change_mask': target, 'image_name': f"{dataset}/images/{event_full_name}", 
+            #                        'event_name': event_name})
+            #process = psutil.Process(os.getpid())
+            splits[dataset].append({
+                't1_image': pre_img_rel_path,
+                't2_image': post_img_rel_path,
+                'change_mask': target_img_rel_path,
+                'image_name': f"{dataset}/images/{event_full_name}",
+                'event_name': event_name
+            })
 
-            pre_img = tiff.imread(pre_img_path)
-            post_img = tiff.imread(post_img_path)
-            target = tiff.imread(target_path)
-
-            splits[dataset].append({'t1_image': pre_img, 't2_image': post_img, 
-                                    'change_mask': target, 'image_name': f"{dataset}/images/{dis_name_code}", 
-                                    'event_name': event_name})
-    
+            #mem_gb = process.memory_info().rss / 1024**3  # Convert bytes to GB
+            #
+            #tqdm.write(f" Memory usage: {mem_gb:.2f} GB")    
     features = Features({
         't1_image': Image(),
         't2_image': Image(), 
@@ -141,7 +157,8 @@ def upload_to_hf(data_dir, repo_name = "BRIGHT-XView2Format"):
 
     dataset_hf = DatasetDict({
         'train': Dataset.from_list(splits['train'], features=features),
-        'val': Dataset.from_list(splits['val'], features=features)
+        'val': Dataset.from_list(splits['val'], features=features),
+        'test': Dataset.from_list(splits['test'], features=features)        
     }) 
 
     dataset_hf.push_to_hub(repo_name, private=False)
@@ -153,7 +170,8 @@ def main():
 
     parser_rearrange_bright = subparsers.add_parser('rearrange_bright', help='Rearrange bright dataset to follow torchange xview2 structure')
     parser_rearrange_bright.add_argument('--bright_dir', type=str, required=True, help='Directory containing the bright dataset.')
-    parser_rearrange_bright.add_argument('--data_dir', type=str, required=True, help='Directory to save the rearranged dataset.')
+    parser_rearrange_bright.add_argument('--data_dir', type=str, required=True, help='Directory to save the rearranged dataset.')    
+    parser_rearrange_bright.add_argument('--split', type=str, required=True, help='Data split')
 
     parser_upload = subparsers.add_parser('upload_to_hf', help='Upload dataset to Hugging Face Hub.')
     parser_upload.add_argument('--data_dir', type=str, required=True, help='Directory containing the dataset.')
@@ -166,7 +184,7 @@ def main():
     args = parser.parse_args()
     
     if args.command == 'rearrange_bright':
-        rearrange_bright(args.bright_dir, args.data_dir)
+        rearrange_bright(args.bright_dir, args.data_dir, args.split)
     elif args.command == 'upload_to_hf':
         upload_to_hf(args.data_dir, args.repo_name)
     elif args.command == 'create_class_finetune':
